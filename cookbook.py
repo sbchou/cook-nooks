ffd352f82ea45e8bdf443029811e2b7abeb066e7
Sophie Chou <sbchou@gmail.com>
2012-01-14 18:34:29 -0500

    pure Diskussy

--- cookbook.py
+++ cookbook.py
@@ -18,18 +18,33 @@ class Recipe(db.Model):
     author = db.UserProperty()
     content = db.StringProperty(multiline=True)
     date = db.DateTimeProperty(auto_now_add=True)
+    cookbook = db.StringProperty(required = True)
+
+class MainPage(webapp.RequestHandler):
+	def get(self):
+		error_state = False
+		no_errors = True
+		values = {'error_state': error_state, 'no_errors': no_errors}
+		self.response.out.write(template.render("main.html", values))
+	
+	def post(self):
+		resource_name = str(random.randint(1000000000, 9999999999))
+		if bool(self.request.get('message')) == False:
+			error_state = True
+			no_errors = False
+			values = {'error_state': error_state, 'no_errors': no_errors}
+			self.response.out.write(template.render("main.html", values))
+        
+		else:
+			new_post = Post(message = self.request.get('message'), who = self.request.get('who'), discussion_id = resource_name)
+			new_post.put()
+			self.re
   
-
-
-def cookbook_key(cookbook_name=None):
-  """Constructs a datastore key for a Cookbook entity with cookbook_name."""
-  return db.Key.from_path('Cookbook', cookbook_name or 'default_cookbook')    
 
 class Cookbook(webapp2.RequestHandler):
     def get(self):
         cookbook_name=self.request.get('cookbook_name')
-        recipes_query = Recipe.all().ancestor(
-                                                  cookbook_key(cookbook_name)).order('-date')
+        recipes_query = Recipe.all().order('-date')
         recipes = recipes_query.fetch(10)
         
         if users.get_current_user():
