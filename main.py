6b164d5cf8a8356826102d61fd34da96ffb14084
Sophie Chou <sbchou@gmail.com>
2012-01-15 16:41:04 -0500

    made rough initial changes, probably lots of bugs

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ from google.appengine.ext.db import GqlQuery
 
 
 class Cookbook(db.Model):
+	#you can leave author blank if you must, cookbooks are ID'd by name
 	author = db.StringProperty(required = False)
 	name = db.StringProperty(required = True)
 	date = db.DateTimeProperty(auto_now_add = True)
+	#fix this
+	cookbook_id = db.StringProperty(required = True)
 
 #defines single model for program, a recipe has content, author, cookbook_id , date
 class Recipe(db.Model):
-	title = db.StringProperty(required = False)
-	content = db.StringProperty(required = False)
+	title = db.StringProperty(required = True)
+	content = db.StringProperty(required = True)
 	date = db.DateTimeProperty(auto_now_add = True)
 	
 #handler for the main page, renders opening template and creates a recipe object when post is clicked
@@ -31,7 +34,7 @@ class MainPage(webapp.RequestHandler):
 	
 	def post(self):
 		#gotta fix this
-		cookbook_name = str(random.randint(1000000000, 9999999999))
+		random_id = str(random.randint(1000000000, 9999999999))
 		
 		#a cookbook must have a name since that's how we identify it
 		if bool(self.request.get('cookbookName')) == False:
@@ -42,49 +45,59 @@ class MainPage(webapp.RequestHandler):
 			
 		else:
 			#create a new cookbook
-			new_cookbook = Cookbook(author = self.request.get('author'), name = self.request.get('cookbookName'))
+			cookbook_name = self.request.get('cookbookName')
+			new_cookbook = Cookbook(author = self.request.get('author'), name = cookbook_name, cookbook_id = random_id)
 			new_cookbook.put()
-			self.redirect('/' + cookbook_name)
+			self.redirect('/' + random_id)
 
 
 #handler for cookbook page--searches all recipes and orders by date, then finds all the recipes with the correct cookbook_id 	
 class CookbookPageHandler(webapp.RequestHandler):
 	
 	def get(self, id):
-		query = Recipe.all().filter('cookbook_id =', id).order('-date')
+		cookbook_query = Cookbook.all().filter('cookbook_id =', id)
+		myCookbook = cookbook_query.get()
+		recipe_query = Recipe.all().ancestor(myCookbook).order('-date')
 		recipes = []
-		for item in query:
+		for item in recipe_query:
 			recipes.append(item)		
-		values = {'recipes' : recipes}
+		values = {'myCookbook': myCookbook, 'recipes' : recipes}
 		self.response.out.write(template.render("cookbook.html", values))
 		
 	def post(self, id):
-		if bool(self.request.get('content')) == False:
-			person = self.request.get('author')
-			query = Recipe.all().filter('cookbook_id =', id).order('-date')
+		if bool(self.request.get('title')) == False:
+			#person = self.request.get('author')
+			cookbook_query = Cookbook.all().filter('cookbook_id =', id)
+			myCookbook = cookbook_query.get()
+			recipe_query = Recipe.all().ancestor(myCookbook).order('-date')
 			recipes = []
-			for item in query:
+			for item in recipe_query:
 				recipes.append(item)		
 			error_state = True
-			values = {'recipes' : recipes, 'person': person, 'error_state': error_state}
+			values = {'recipes' : recipes, 'error_state': error_state}
 			self.response.out.write(template.render("cookbook.html", values))
 		else:
-			new_recipe = Recipe(content = self.request.get('content'), author = self.request.get('author'), cookbook_id  = id)
+			cookbook_query = Cookbook.all().filter('cookbook_id =', id)
+			myCookbook = cookbook_query.get()
+			new_recipe = Recipe(parent = myCookbook, title = self.request.get('title'), content = self.request.get('content'))
 			new_recipe.put()
-			person = self.request.get('author')
-			query = Recipe.all().filter('cookbook_id =', id).order('-date')
+			
+			recipe_query = Recipe.all().ancestor(myCookbook).order('-date')
 			recipes = []
-			for item in query:
+			for item in recipe_query:
 				recipes.append(item)		
+				
 			error_state = False
-			values = {'recipes' : recipes, 'person': person, 'error_state': error_state}
+			values = {'recipes' : recipes, 'error_state': error_state}
 			self.response.out.write(template.render("cookbook.html", values))
 		
 class RecipeHandler(webapp.RequestHandler):
 	def get(self, id):
-		query = Recipe.all().filter('cookbook_id =', id).order('-date')
+		cookbook_query = Cookbook.all().filter('cookbook_id =', id)
+		myCookbook = cookbook_query.get()
+		recipe_query = Recipe.all().ancestor(myCookbook).order('-date')
 		recipes = []
-		for item in query:
+		for item in recipe_query:
 			recipes.append(item)		
 		self.response.out.write(template.render("chatscreen.html", {'recipes':recipes}))
 
