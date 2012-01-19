95d177f3bca92503dcc5dd5ea7df0eeff8bd7ab2
Sophie Chou <sbchou@gmail.com>
2012-01-18 19:46:44 -0500

    whitespace replaced w/dash in URL

--- README.txt
+++ README.txt
@@ -3,14 +3,16 @@ www.cook-nooks.appspot.com
 
 A very simple and user-friendly web app for chefs and foodies to create a Cook-Nook: a personal log of their recipes in their own little corner of the web. Features a single-column Tumblr-style layout.
 
-Created because I wanted an easier and more aesthetically appealing way to store my collection of favorite recipes on the internet than a Google Doc. Existing options seemed to be more focused on sharing recipes and social networking, whereas I needed a personal journal, to share (or not to share!) at my own disposal.
+Created because I wanted an easier and more aesthetically appealing way to store my collection of favorite recipes on the internet than a Google Doc. Existing options seemed to be more focused on sharing recipes and social networking, whereas I needed a personal journal, to share (or not to share!) at my own disposal. Cook-Nooks can be accessed directly via its URL and its super-simple recipe posting makes it easier on smartphones.
 
 In short: a recipe box Hemingway would approve of.
 
 WARNING:
-Cook-Nooks is still evolving, so use with caution! Currently, there is no delete recipe feature.
+Cook-Nooks is still evolving, so use with caution! Currently, any person can post to another's wall! Also, there is no delete recipe feature.
 
 To-Do:
+
+Delete recipe feature
 Print recipe feature
 Email recipe feature
-Delete recipe feature
+Filter, order
--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class Cookbook(db.Model):
 	#you can leave author blank if you must, cookbooks are ID'd by name
 	author = db.StringProperty(required = False)
 	name = db.StringProperty(required = True)
+	#the difference b/t the name and the key name is that key name replaces the whitespaces with dashes
 	date = db.DateTimeProperty(auto_now_add = True)
 
 #defines single model for program, a recipe has content, author, date
@@ -41,13 +42,14 @@ class MainPage(webapp.RequestHandler):
 			self.response.out.write(template.render("main.html", values))
 			
 		else:
-			cookbook_name = self.request.get('cookbookName')
+			#the cookbook key is constructed out of its name, except replacing whitespace w/dashes for URL purposes
+			cookbook_key = self.request.get('cookbookName').strip().replace(' ', '-')
+			#if no cookbook with this id exists make new cookbook, with the FORMATTED name (no whitespace!) as its key
 			
-			#if no cookbook with this id exists make new cookbook, with the name as its key
-			if Cookbook.get_by_key_name(cookbook_name) == None:
-				new_cookbook = Cookbook(key_name = cookbook_name, author = self.request.get('author'), name = cookbook_name)
+			if Cookbook.get_by_key_name(cookbook_key) == None:
+				new_cookbook = Cookbook(key_name = cookbook_key, author = self.request.get('author'), name = self.request.get('cookbookName'))
 				new_cookbook.put()
-				self.redirect('/' + cookbook_name)
+				self.redirect('/' + cookbook_key)
 			
 			#else show error message
 			else:
@@ -61,10 +63,8 @@ class MainPage(webapp.RequestHandler):
 class CookbookPageHandler(webapp.RequestHandler):
 	
 	def get(self, id):
-		cookbook_name = id
-		myCookbook = Cookbook.get_by_key_name(cookbook_name)
-		#cookbook_query = Cookbook.all().filter('cookbook_id =', id)
-		#myCookbook = cookbook_query.get()
+		cookbook_key = id
+		myCookbook = Cookbook.get_by_key_name(cookbook_key)
 		recipe_query = Recipe.all().ancestor(myCookbook).order('-date')
 		recipes = []
 		for item in recipe_query:
@@ -74,10 +74,8 @@ class CookbookPageHandler(webapp.RequestHandler):
 		
 	def post(self, id):
 		if bool(self.request.get('title')) == False:
-			#cookbook_query = Cookbook.all().filter('cookbook_id =', id)
-			#myCookbook = cookbook_query.get()
-			cookbook_name = id
-			myCookbook = Cookbook.get_by_key_name(cookbook_name)
+			cookbook_key = id
+			myCookbook = Cookbook.get_by_key_name(cookbook_key)
 			recipe_query = Recipe.all().ancestor(myCookbook).order('-date')
 			recipes = []
 			for item in recipe_query:
@@ -86,10 +84,8 @@ class CookbookPageHandler(webapp.RequestHandler):
 			values = {'recipes' : recipes, 'error_state': error_state}
 			self.response.out.write(template.render("cookbook.html", values))
 		else:
-			#cookbook_query = Cookbook.all().filter('cookbook_id =', id)
-			#myCookbook = cookbook_query.get()
-			cookbook_name = id
-			myCookbook = Cookbook.get_by_key_name(cookbook_name)
+			cookbook_key = id
+			myCookbook = Cookbook.get_by_key_name(cookbook_key)
 			new_recipe = Recipe(parent = myCookbook, title = self.request.get('title'), content = self.request.get('content'))
 			new_recipe.put()
 			
@@ -104,10 +100,8 @@ class CookbookPageHandler(webapp.RequestHandler):
 		
 class RecipeHandler(webapp.RequestHandler):
 	def get(self, id):
-		#cookbook_query = Cookbook.all().filter('cookbook_id =', id)
-		#myCookbook = cookbook_query.get()
-		cookbook_name = id
-		myCookbook = Cookbook.get_by_key_name(cookbook_name)
+		cookbook_key = id
+		myCookbook = Cookbook.get_by_key_name(cookbook_key)
 		recipe_query = Recipe.all().ancestor(myCookbook).order('-date')
 		recipes = []
 		for item in recipe_query:
@@ -115,7 +109,7 @@ class RecipeHandler(webapp.RequestHandler):
 		self.response.out.write(template.render("chatscreen.html", {'recipes':recipes}))
 
 def main():
-	app = webapp.WSGIApplication([(r'/$', MainPage), (r'/mess/(\w*)', RecipeHandler), (r'/(\w*)', CookbookPageHandler)], debug=True)
+	app = webapp.WSGIApplication([(r'/$', MainPage), (r'/mess/(\w*)', RecipeHandler), (r'/([A-Za-z0-9-]+)', CookbookPageHandler)], debug=True)
 	wsgiref.handlers.CGIHandler().run(app)
 
 #implements program
